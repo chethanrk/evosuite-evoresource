3180e22a501fcf7b2366ca96b0056b43bf131783
Schlage Michaela <michaela.schlage@evorait.com>
2022-03-16 07:09:26 +0000

    Load Resource data part1

--- webapp/controller/ResourcePlanGantt.controller.js
+++ webapp/controller/ResourcePlanGantt.controller.js
@@ -1,10 +1,28 @@
 sap.ui.define([
 	"com/evorait/evosuite/evoresource/controller/BaseController",
-	"com/evorait/evosuite/evoresource/model/formatter"
-], function (BaseController, formatter) {
+	"sap/ui/core/mvc/OverrideExecution",
+	"com/evorait/evosuite/evoresource/model/formatter",
+	"sap/base/util/deepClone",
+	"com/evorait/evosuite/evoresource/model/models"
+], function (BaseController, OverrideExecution, formatter, deepClone, models) {
 	"use strict";
 
 	return BaseController.extend("com.evorait.evosuite.evoresource.controller.ResourcePlanGantt", {
+
+		metadata: {
+			// extension can declare the public methods
+			// in general methods that start with "_" are private
+			methods: {
+				onChangeTimeMode: {
+					public: true,
+					final: false,
+					overrideExecution: OverrideExecution.After
+				}
+			}
+		},
+
+		oOriginData: null,
+		oPlanningModel: null,
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
@@ -12,23 +30,63 @@ sap.ui.define([
 		 * @memberOf com.evorait.evosuite.evoresource.controller.ResourcePlanningMain
 		 */
 		onInit: function () {
-			/*this.oZoomStrategy = this.getView().byId("idGanttZoom");
+			this.oZoomStrategy = this.getView().byId("idResourcePlanGanttZoom");
 			this.oZoomStrategy.setTimeLineOptions(formatter.getTimeLineOptions());
-			this.oZoomStrategy.setTimeLineOption(formatter.getTimeLineOptions("DAY"));*/
+			this.oZoomStrategy.setTimeLineOption(formatter.getTimeLineOptions("DAY"));
 
 			//idPageResourcePlanningWrapper
+			this.oOriginData = {};
+			this.oPlanningModel = models.createHelperModel(deepClone(this.oOriginData));
+			this.getView().setModel(this.oPlanningModel, "ganttPlanningModel");
 		},
+
+		/* =========================================================== */
+		/* Events                                                      */
+		/* =========================================================== */
 
 		/**
 		 * Set new time line options for Gantt time horizon
-		 * @param {obejct} oEvent - change event of filter view mode
+		 * @param {object} oEvent - change event of filter view mode
 		 */
 		onChangeTimeMode: function (oEvent) {
-			/*var mParam = oEvent.getParameters(),
+			var mParam = oEvent.getParameters(),
 				sKey = mParam.selectedItem.getKey();
-
 			this.oZoomStrategy.setTimeLineOption(formatter.getTimeLineOptions(sKey));
-			this._setBackgroudShapes(sKey);*/
+			this._setBackgroudShapes(sKey);
+		},
+
+		/**
+		 * When filterbar was initialized then get all filters and send backend request
+		 * @param {object} oEvent - change event of filterbar
+		 */
+		onInitializedSmartVariant: function (oEvent) {
+			this._getResourceData();
+		},
+
+		/**
+		 * When Go button in filterbar was pressed then get all filters and send backend request
+		 * @param {object} oEvent - change event of filterbar
+		 */
+		onSearch: function () {
+			this._getResourceData();
+		},
+
+		/* =========================================================== */
+		/* internal methods                                            */
+		/* =========================================================== */
+
+		/**
+		 * get all filters from SmartFilterbar and read data with filters
+		 * set result with deepClone to Json Model
+		 */
+		_getResourceData: function () {
+			var oFilterBar = this.getView().byId("idPageResourcePlanningSmartFilterBar"),
+				sUri = "/" + this.getModel("templateProperties").getProperty("/ganttConfigs/entitySet");
+
+			//sUri, aFilters, mUrlParams
+			this.getOwnerComponent().readData(sUri, oFilterBar.getFilters(), null).then(function (aResult) {
+				console.log(aResult);
+			});
 		}
 	});
 });
