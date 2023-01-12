433722759c4401742360ba5d6acf0ff2b4da469f
Sagar Poojari <sagar.poojari@evorait.com>
2023-01-12 04:19:12 +0000

    Revert last commit changes

--- webapp/controller/ResourcePlanGantt.controller.js
+++ webapp/controller/ResourcePlanGantt.controller.js
@@ -352,11 +352,6 @@ sap.ui.define([
 					public: true,
 					final: false,
 					overrideExecution: OverrideExecution.After
-				},
-				onPressCancelAssignment: {
-					public: true,
-					final: false,
-					overrideExecution: OverrideExecution.Before
 				}
 			}
 		},
@@ -2074,16 +2069,6 @@ sap.ui.define([
 				this.oPlanningModel.setProperty("/multiCreateData/EndDate", oEndDate);
 				this.oPlanningModel.setProperty("/multiCreateData/EffectiveEndDate", oEndDate);
 
-			}
-		},
-
-		/**
-		 * 
-		 * 
-		 */
-		onPressCancelAssignment: function (oEvent) {
-			if (this._oPlanningPopover) {
-				this._oPlanningPopover.close();
 			}
 		},
 		/* =========================================================== */
